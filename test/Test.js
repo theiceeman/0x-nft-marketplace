2dc44277df52fd32a32b216911639b598b20d215
Ebube <okorieebube1@gmail.com>
2022-04-28 18:38:21 +0100

    Issue with account sending the `filling order`

--- test/Test.js
+++ test/Test.js
@@ -25,19 +25,21 @@ describe("NftSwap", function () {
     BUYER_SIGNER = SELLER.connect(PROVIDER);
 
     /* USD = await ethers.getContractFactory("USD");
-    usd = await USD.deploy();
-    usd.connect(SELLER).transfer(BUYER.address, parseEther("50000"));
+    usd = await USD.connect(BUYER).deploy();
 
     BOREDPUNK = await ethers.getContractFactory("BOREDPUNK");
     boredPunk = await BOREDPUNK.deploy();
     boredPunk.connect(SELLER).safeMint(SELLER_SIGNER.address); */
 
-    // console.log('boredPunk',boredPunk.address)
-    // console.log('usd',usd.address)
+    // console.log("boredPunk", boredPunk.address);
+    // console.log("usd", usd.address);
 
     // Rinkeby Deployment...
-    usd = { address: "0x20de04b91238d7927B61f07deA051A0042F4e9f1" };
-    boredPunk = { address: "0x7DC5442C68C2C89a6A80BEF37ABCF1b08E6a4556" };
+    usd = { address: "0x2a70bfFa2e2F65c42dE7086356C8c9D5Ef7a7D45" };
+    boredPunk = { address: "0x281BdCb7552bC606569fAeD0406407c79Df56Ca9" };
+
+    Usd = await ethers.getContractAt("IERC20", usd.address);
+    console.log("usd.balanceOf", await Usd.balanceOf(BUYER.address));
 
     CHAIN_ID = 4; // Hardhat is 31337, Ganache is 1337, Rinkeby is 4, Ropsten is 3
 
@@ -45,35 +47,52 @@ describe("NftSwap", function () {
   });
   it("Scenario: User A wants to sell their BoredPunk for 420 USD", async function () {
     // Set up the assets we want to swap (CryptoPunk #69 and 420 WETH)
-    const BOREDPUNK = {
+    const SELLER_ASSET = {
       tokenAddress: boredPunk.address,
       tokenId: "0",
       type: "ERC721", // 'ERC721' or 'ERC1155'
     };
-    const FOUR_HUNDRED_TWENTY_WETH = {
+    const BUYER_ASSET = {
       tokenAddress: usd.address,
-      amount: "420000000000000000000",
+      amount: "4200",
       type: "ERC20",
     };
 
     // [Part 1: Maker (owner of the Punk) creates trade]
     var nftSwapperMaker = new NftSwap(PROVIDER, SELLER_SIGNER, CHAIN_ID);
     const order = nftSwapperMaker.buildOrder(
-      [BOREDPUNK],
-      [FOUR_HUNDRED_TWENTY_WETH],
+      [SELLER_ASSET],
+      [BUYER_ASSET],
       SELLER.address
     );
 
     const signedOrder = await nftSwapperMaker.signOrder(order, SELLER.address);
     console.log({ signedOrder });
 
-    var nftSwapperMaker = new NftSwap(PROVIDER, BUYER_SIGNER, CHAIN_ID);
+    var _nftSwapperMaker = new NftSwap(PROVIDER, BUYER_SIGNER, CHAIN_ID);
 
-    // Uncomment to actually fill order
-    const tx = await nftSwapperMaker.fillSignedOrder(signedOrder, undefined, {
+    // Check if we need to approve the NFT for swapping
+    const approvalStatusForUserB = await _nftSwapperMaker.loadApprovalStatus(
+      BUYER_ASSET,
+      BUYER.address
+    );
+    // If we do need to approve NFT for swapping, let's do that now
+    if (!approvalStatusForUserB.contractApproved) {
+      const approvalTx = await _nftSwapperMaker.approveTokenOrNftByAsset(
+        BUYER_ASSET,
+        BUYER.address
+      );
+      const approvalTxReceipt = await approvalTx.wait();
+      console.log(
+        `Approved ${usd.address} contract to swap with 0x. TxHash: ${approvalTxReceipt.transactionHash})`
+      );
+    }
+
+    // Taker fills order
+    const tx = await _nftSwapperMaker.fillSignedOrder(signedOrder, undefined, {
       gasPrice,
       gasLimit: "500000",
-      value: parseEther("0.05"), //  Rinkeby still has protocol fees, so we give it a little bit of ETH so its happy.
+      value: parseEther("0.01"), //  Rinkeby still has protocol fees, so we give it a little bit of ETH so its happy.
     });
 
     const txReceipt = await tx.wait();
