cd01592e2e476bbe4fc8a69e488e4e96efcaa32f
Ebube <okorieebube1@gmail.com>
2022-05-09 09:39:41 +0100

    Converting tokenURI json to base64

--- contracts/BoredPunk.sol
+++ contracts/BoredPunk.sol
@@ -2,12 +2,28 @@
 pragma solidity ^0.8.4;
 
 import "erc721a/contracts/ERC721A.sol";
+import {Base64} from "./libraries/Base64.sol";
 
 contract BOREDPUNK is ERC721A {
-  constructor() ERC721A("BoredPunk", "BOREDPUNK") {}
+  string public baseTokenUri;
 
-  function mint(uint256 quantity) external payable {
-    // _safeMint's second argument now takes in a quantity, not a tokenId.
-    _safeMint(msg.sender, quantity);
-  }
+
+    constructor() ERC721A("BoredPunk", "BOREDPUNK") {}
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenUri;
+    }
+
+    function mint(uint256 quantity, bytes32 data) external payable {
+        // Get all the JSON metadata in place and base64 encode it.
+        string memory json = Base64.encode(
+            bytes(string(abi.encodePacked(data)))
+        );
+        // Just like before, we prepend data:application/json;base64, to our data.
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+        // _safeMint's second argument now takes in a quantity, not a tokenId.
+        _safeMint(msg.sender, quantity);
+    }
 }
